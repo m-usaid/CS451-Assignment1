41efc8d8f99852d466c634e43763b84bf3aa4ff0
musaid <mr04302@st.habib.edu.pk>
2021-02-11 00:07:12 +0500

    some more minimization functionality

--- EA_tsp.py
+++ EA_tsp.py
@@ -2,7 +2,7 @@ import tsplib95
 import random
 import matplotlib.pyplot as plt
 import networkx as nx
-
+from math import fsum
 
 #############################################
 ######### POPULATION INITIALIZATION ######### 
@@ -76,10 +76,11 @@ def fitnesses_tsp(population, tsp):
     return fitnesses
 
 
-def rel_fit_min(fitnesses):
+def rel_fit_min(population, tsp):
+    fits = fitnesses_tsp(population, tsp)
     rel_fit = []
-    s_fit = sum(fitnesses)
-    rel_fit = [(1/i)*s_fit for i in fitnesses]
+    s_fit = sum(fits)
+    rel_fit = [(1/i)*s_fit for i in fits]
     return rel_fit
 
 def fitness_knpsk():
@@ -97,7 +98,7 @@ def fitnesses_knpsk():
 ######### PARENT SELECTION SCHEMA (MINIMIZATION) ######### 
 ##########################################################
 
-def select_fps_min(population, fitnesses):
+def select_fps(population, fitnesses):
     """Generates parents based on fitness proportional scheme.
 
     Args:
@@ -109,14 +110,12 @@ def select_fps_min(population, fitnesses):
     """
     parents = []
     divs = []
-    rel_fit = rel_fit_min(fitnesses)
-    probs = [rel_fit[i]/sum(rel_fit) for i in range(len(rel_fit))]
+    probs = [fitnesses[i]/sum(fitnesses) for i in range(len(fitnesses))]
 
     s_probs = 0
     for i in range(len(probs)):
         divs.append(s_probs+probs[i])
         s_probs += probs[i]
-    
     while len(parents) < 2:
         r = random.uniform(0, 1)
         for i in range(len(population)):
@@ -136,7 +135,36 @@ def select_bin_tour_min(population, tsp):
         parents.append(best)
     return parents 
 
+def rank_select(population, tsp):
+    parents = []
+    pop = sorted(population, key=lambda fit: fitness_tsp(fit, tsp), reverse=True)
+    N = len(pop)
+    totalrank = N*(N+1) / 2
+    
+    probs = []
+    divs = []
+    s_probs = 0
+    
+    for i in range(1, len(pop)+1):
+        probs.append(i/totalrank)
 
+    for i in range(len(probs)):
+        divs.append(s_probs+probs[i])
+        s_probs += probs[i]
+    while len(parents) < 2:
+        r = random.uniform(0, 1)
+        for i in range(len(pop)):
+            if r > divs[i] and r < divs[i+1]:
+                parents.append(pop[i]) 
+                continue 
+    return parents 
+        
+
+# def select_trunc_min(population, num, tsp):
+#     surv = []
+#     surv = sorted(population, key=lambda fit: fitness_tsp(fit, tsp))
+#     surv = surv[:num]
+#     return surv
 
 #################################################################
 #################################################################
@@ -166,9 +194,12 @@ def create_offspring(population, fitnesses, parent_sel, tsp, num_offspring = 10)
     while len(offspring) < num_offspring:
         parents = []
         if parent_sel == "fps":
-            parents = select_fps_min(population, fitnesses)
+            rel_fit = rel_fit_min(population, tsp)
+            parents = select_fps(population, rel_fit)
         elif parent_sel == "tourny":
             parents = select_bin_tour_min(population, tsp)
+        elif parent_sel == "rank":
+            parents = rank_select(population, tsp)
         child = []
         pot_parents = random.sample(parents, 2)
         child = crossover(pot_parents)
@@ -191,7 +222,7 @@ def mutate(chrom):
     return chrom
 
 
-def variate(offspring, p_m = 0.3):
+def variate(offspring, p_m):
     for i in range(len(offspring)):
         if random.uniform(0,1) < p_m:
             offspring[i] = mutate(offspring[i])  
@@ -206,15 +237,15 @@ def variate(offspring, p_m = 0.3):
 #############################################
 
 ## NOT WORKING
-def selectsurvivors_fps(population, fitnesses, pop_size=30):
+def selectsurvivors_fps(population, fitnesses, pop_size):
     nextgen = []
     while len(nextgen) < pop_size:
-        tmp = select_fps_min(population, fitnesses)
+        tmp = select_fps(population, fitnesses)
         nextgen.append(tmp[0])
         nextgen.append(tmp[1])
     return nextgen
 
-def survivor_trunc(population,tsp, pop_size=30):
+def survivor_trunc_min(population,tsp, pop_size):
     """Truncation based survivor scheme.
 
     Args:
@@ -230,22 +261,36 @@ def survivor_trunc(population,tsp, pop_size=30):
     surv = surv[:pop_size]
     return surv
 
+def survior_rank_based():
+    pass
+
+def survivor_select_min(population, fitnesses, tsp, pop_size, scheme):
+    nextgen = []
+    rel_fit = rel_fit_min(population, tsp)
+    if scheme == "trunc":
+        nextgen = survivor_trunc_min(population, tsp, pop_size)
+    elif scheme == "fps":
+        nextgen = selectsurvivors_fps(population, rel_fit, pop_size)
+    return nextgen
+
 #################################################################
 #################################################################
 
 
-def EA(tsp, parent_sel, num_gens = 5000, p_m=0.5, pop_size=30):
+def EA(tsp, parent_sel, surv_sel, num_gens = 100, p_m=0.3, pop_size=30):
     pop = init_tsp_pop(tsp_instance, pop_size)
     t = 0
     while t < num_gens:
         fits = fitnesses_tsp(pop, tsp)
         offspring = create_offspring(pop, fits, parent_sel, tsp)
-        offspring = variate(offspring)
+        offspring = variate(offspring, p_m)
         pop += offspring 
         fits = fitnesses_tsp(pop, tsp)
-        pop = survivor_trunc(pop, tsp)
+        #pop = survivor_trunc_min(pop, tsp, pop_size)
+        pop = survivor_select_min(pop, fits, tsp, pop_size, surv_sel)
         t += 1
-        print(min(fits))
+        #print("generation {}: {}".format(t, sum(fits)/len(fits)))
+        print("generation {}: {}".format(t, min(fits)))
 
 ####################################################################
 ####################################################################
@@ -255,7 +300,13 @@ def EA(tsp, parent_sel, num_gens = 5000, p_m=0.5, pop_size=30):
 # p = [[1,5,4,3,8,9], [3,7,2,5,4,5], [3,7,2,1,4,5]]
 
 tsp_instance = tsplib95.load('wi29.tsp')
-#pop = init_tsp_pop(tsp_instance, 50)
+
+pop = init_tsp_pop(tsp_instance, 50)
+fit = fitnesses_tsp(pop, tsp_instance)
+#print(select_fps_min(pop, fit))
+newpop = selectsurvivors_fps(pop, fit, 30)
+#print(fitnesses_tsp(newpop, tsp_instance))
+#print(rank_select(pop, tsp_instance))
 #print(select_bin_tour_min(pop, tsp_instance))
 # #survivor_trunc(pop, tsp_instance)
-EA(tsp_instance, "tourny")
+EA(tsp_instance, "rank", "trunc")
