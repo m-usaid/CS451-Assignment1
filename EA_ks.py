6614a4e783bf2b270f54d46cc0db36147b3a2d3f
musaid <mr04302@st.habib.edu.pk>
2021-02-14 19:35:13 +0500

    defaut parameters created

--- EA_ks.py
+++ EA_ks.py
@@ -206,7 +206,7 @@ def survival_selection_ksp(sortedPopulation,killCount,sparedChromos):
 #################################################################
 #################################################################
 
-def EA_ks(filename,popSize,generations,reproductionRate,sparedChromos,mutationRate,numOfMutations=1):
+def EA_ks(filename,generations,sparedChromos=25, popSize=50, num_offspring=50,p_m = 0.5,numOfMutations=1):
 
     # Initialization
     maxWeight, items = load_items_ks(filename)
@@ -219,7 +219,7 @@ def EA_ks(filename,popSize,generations,reproductionRate,sparedChromos,mutationRa
         # Fitness Evaluation (In this case this has been stored in the poppulation already)
         fitnesses = fitnesses_ks(pop)
         # Parent Selection
-        parents = select_fps(pop,fitnesses,2*reproductionRate)
+        parents = select_fps(pop,fitnesses,2*num_offspring)
         # Make parents pairs for reproduction
         parentPairs = [parents[x:x+2] for x in range(0, len(parents), 2)]
 
@@ -232,31 +232,27 @@ def EA_ks(filename,popSize,generations,reproductionRate,sparedChromos,mutationRa
         
         # Mutation
         for i in range(len(children)):
-            if (random.random() < mutationRate):
+            if (random.random() < p_m):
                 children[i] = mutation(children[i],items,maxWeight,numOfMutations)
 
         # Add Offspring to poppulation
 
         pop.extend(children)
 
-        # print("Pop before sort")
-        # for p in pop:
-        #     print(p)
-
         # Sort poppulation based on fitness
         pop.sort(key=lambda x: x[0],reverse=True)
 
         # Kill Excess Chromosones based on survival selection
-        pop = survival_selection_ksp(pop,reproductionRate,sparedChromos)
+        pop = survival_selection_ksp(pop,num_offspring,sparedChromos)
         # Sort poppulation based on fitness
 
         pop.sort(key=lambda x: x[0],reverse=True)
         SumFitnesess = sum(x[0] for x in pop)
         BF_values.append(pop[0][0])
         AF_values.append(SumFitnesess/len(pop))
-        print("Generation",g,":","BF =",pop[0][0],"LF = ",pop[-1][0],"AF =", SumFitnesess/len(pop))
+        #print("Generation",g,":","BF =",pop[0][0],"LF = ",pop[-1][0],"AF =", SumFitnesess/len(pop))
         
-    print("Optimal Chromosome: ", pop[0])
+    #print("Optimal Chromosome: ", pop[0])
 
     return (BF_values,AF_values)
 
